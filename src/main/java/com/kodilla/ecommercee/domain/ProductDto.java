90bd37aa31a199a447ed261b7776252b22a3307a
Pawel Krzywda <paw.krzywda@gmail.com>
2022-10-23 20:20:25 +0200

    JDP221101-1 ProductController - initial implementation.

---
+++ src/main/java/com/kodilla/ecommercee/domain/ProductDto.java
@@ -0,0 +1,17 @@
+package com.kodilla.ecommercee.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class ProductDto {
+    private Long id;
+    private String name;
+    private String description;
+    private Integer quantity;
+    private BigDecimal price;
+}
