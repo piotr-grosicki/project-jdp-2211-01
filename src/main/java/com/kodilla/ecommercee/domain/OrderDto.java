99a7c92621b25baeb4e965b76cc6f5a81016fcf3
Damian1457 <damianwasik.coach@gmail.com>
2022-10-23 15:23:14 +0200

    JDP221101-4 Initial implementation of endpoints

---
+++ src/main/java/com/kodilla/ecommercee/domain/OrderDto.java
@@ -0,0 +1,20 @@
+package com.kodilla.ecommercee.domain;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+
+@Data
+@AllArgsConstructor
+@Builder
+public class OrderDto {
+    private int orderId;
+    private String deliveryAdress;
+    private String deliveryMethod;
+    private LocalDateTime ordearDatatime;
+    private BigDecimal value;
+}
