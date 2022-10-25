92c02482b49c74dafd344e9d82b55b6ce4c97b3e
Pawel Krzywda <paw.krzywda@gmail.com>
2022-10-25 20:56:55 +0200

    Cart Entity

---
+++ src/main/java/com/kodilla/ecommercee/entities/Cart.java
@@ -0,0 +1,40 @@
+package com.kodilla.ecommercee.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity(name= "carts")
+public class Cart {
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name="cart_id", unique = true)
+    private long id;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id" )
+    private User user;
+
+    @OneToMany(cascade = CascadeType.PERSIST)
+    @JoinTable(
+            name = "product_cart",
+            joinColumns = {@JoinColumn(name = "cart_id", referencedColumnName = "cart_id")},
+            inverseJoinColumns = {@JoinColumn(name = "product_id", referencedColumnName = "product_id")}
+    )
+    private List<Product> listOfProducts;
+
+    public Cart(User user, List<Product> listOfProducts) {
+        this.user = user;
+        this.listOfProducts = listOfProducts;
+    }
+}
