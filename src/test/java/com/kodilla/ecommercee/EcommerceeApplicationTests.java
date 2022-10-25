b36dd03632b81389a72d67254591b02ccc415788
Sebastian Boron <sebastian.boron8@gmail.com>
2022-10-25 12:09:19 +0200

    Order Entity

--- src/test/java/com/kodilla/ecommercee/EcommerceeApplicationTests.java
+++ src/test/java/com/kodilla/ecommercee/EcommerceeApplicationTests.java
@@ -1,16 +1,38 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.entities.Cart;
+import com.kodilla.ecommercee.entities.Order;
+import com.kodilla.ecommercee.entities.User;
+import com.kodilla.ecommercee.repository.OrderDao;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class EcommerceeApplicationTests {
 
+    @Autowired
+    OrderDao orderDao;
     @Test
     public void contextLoads() {
+    }
+
+    @Test
+    public void addingNewOrder() {
+
+        BigDecimal bigDecimal = BigDecimal.valueOf(100L);
+
+        LocalDateTime time = LocalDateTime.of(2121, 1,1, 1, 1);
+        Order order = new Order(21L, "m1", "d1", bigDecimal, time, new Cart(), new User());
+
+
+        orderDao.save(order);
     }
 
 }
