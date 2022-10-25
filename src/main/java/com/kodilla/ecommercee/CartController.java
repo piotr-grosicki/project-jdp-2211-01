95facabc06846f6356dd82cabe9e4e6081f2d5b3
Sebastian Boron <sebastian.boron8@gmail.com>
2022-10-25 13:33:47 +0200

    CartController with code formatted

--- src/main/java/com/kodilla/ecommercee/CartController.java
+++ src/main/java/com/kodilla/ecommercee/CartController.java
@@ -13,49 +13,49 @@ import java.util.List;
 @RestController
 @RequestMapping("/v1/carts")
 @CrossOrigin("*")
-public class  CartController {
+public class CartController {
 
-@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-CartDto createNewCart(@RequestBody CartDto cartDto) {
-    return CartDto.builder()
-            .id(4L)
-            .userId(341L)
-            .build();
-}
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    CartDto createNewCart(@RequestBody CartDto cartDto) {
+        return CartDto.builder()
+                .id(4L)
+                .userId(341L)
+                .build();
+    }
 
-@GetMapping(value ="/{cartId}")
-List<ProductDto> getProductsFromCart(@PathVariable long cartId) throws CartNotFoundException {
-    return Arrays.asList(
-            ProductDto.builder()
-            .id(3L)
-            .name("Test product 1")
-            .build(),
+    @GetMapping(value = "/{cartId}")
+    List<ProductDto> getProductsFromCart(@PathVariable long cartId) throws CartNotFoundException {
+        return Arrays.asList(
+                ProductDto.builder()
+                        .id(3L)
+                        .name("Test product 1")
+                        .build(),
 
-            ProductDto.builder()
-                    .id(33L)
-                    .name("Test product 2")
-                    .build());
-}
+                ProductDto.builder()
+                        .id(33L)
+                        .name("Test product 2")
+                        .build());
+    }
 
-@PutMapping
-CartDto addProductToCart(@RequestParam long cartId, @RequestParam long productId) throws CartNotFoundException, ProductNotFoundException{
-    return CartDto.builder()
-            .id(1)
-            .userId(12)
-            .listOfProducts(Arrays.asList(ProductDto.builder().id(3L).name("Test product").build()))
-            .build();
-}
+    @PutMapping
+    CartDto addProductToCart(@RequestParam long cartId, @RequestParam long productId) throws CartNotFoundException, ProductNotFoundException {
+        return CartDto.builder()
+                .id(1)
+                .userId(12)
+                .listOfProducts(Arrays.asList(ProductDto.builder().id(3L).name("Test product").build()))
+                .build();
+    }
 
-@DeleteMapping
-CartDto removeProductFromCart(@RequestParam long cartId, @RequestParam long productId) throws CartNotFoundException, ProductNotFoundException {
-    return CartDto.builder()
-            .id(2)
-            .userId(122)
-            .listOfProducts(Arrays.asList(ProductDto.builder().id(3L).name("Test product").build()))
-            .build();
-}
+    @DeleteMapping
+    CartDto removeProductFromCart(@RequestParam long cartId, @RequestParam long productId) throws CartNotFoundException, ProductNotFoundException {
+        return CartDto.builder()
+                .id(2)
+                .userId(122)
+                .listOfProducts(Arrays.asList(ProductDto.builder().id(3L).name("Test product").build()))
+                .build();
+    }
 
-@GetMapping(value = "/order")
-void createOrder(@RequestParam long cartId) throws CartNotFoundException{
-}
+    @GetMapping(value = "/order")
+    void createOrder(@RequestParam long cartId) throws CartNotFoundException {
+    }
 }
