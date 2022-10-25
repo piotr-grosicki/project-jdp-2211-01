dc8f1e1e0ae717d66211373610c5cf2e2347868d
Sebastian Boron <sebastian.boron8@gmail.com>
2022-10-25 13:30:54 +0200

    Order Entity and OrderDao added

--- src/main/java/com/kodilla/ecommercee/entities/Order.java
+++ src/main/java/com/kodilla/ecommercee/entities/Order.java
@@ -10,7 +10,7 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "Order")
+@Table(name = "Orders")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
--- src/test/java/com/kodilla/ecommercee/EcommerceeApplicationTests.java
+++ src/test/java/com/kodilla/ecommercee/EcommerceeApplicationTests.java
@@ -16,24 +16,7 @@ import java.time.LocalDateTime;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class EcommerceeApplicationTests {
-
-    @Autowired
-    OrderDao orderDao;
     @Test
     public void contextLoads() {
     }
-
-    @Test
-    public void addingNewOrder() {
-
-        BigDecimal bigDecimal = BigDecimal.valueOf(100L);
-
-        LocalDateTime time = LocalDateTime.of(2121, 1,1, 1, 1);
-        Order order = new Order(21L, "m1", "d1", bigDecimal, time, new Cart(), new User());
-
-
-        orderDao.save(order);
-    }
-
 }
-
