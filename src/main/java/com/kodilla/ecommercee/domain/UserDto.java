01cc69495f820da0ae27d4b96e8d47febfd78f38
unknown <mateuszp.nowak@gmail.com>
2022-10-26 19:56:04 +0200

    UserController and User Dto initial implementation

---
+++ src/main/java/com/kodilla/ecommercee/domain/UserDto.java
@@ -0,0 +1,20 @@
+package com.kodilla.ecommercee.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class UserDto {
+
+    private Long id;
+    private String name;
+    private String surname;
+    private String deliveryAddress;
+    private String login;
+    private String password;
+    private String key;
+    private boolean isActive;
+}
