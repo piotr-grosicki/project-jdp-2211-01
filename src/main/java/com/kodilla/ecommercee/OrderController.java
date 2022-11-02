a1f355e90953043ce29811dc926fa8ddb33e09bd
Damian1457 <damianwasik.coach@gmail.com>
2022-11-02 21:30:17 +0100

    OrderController implementation

--- src/main/java/com/kodilla/ecommercee/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/OrderController.java
@@ -1,69 +1,54 @@
 package com.kodilla.ecommercee;
 
 import com.kodilla.ecommercee.domain.OrderDto;
+import com.kodilla.ecommercee.entities.Order;
 import com.kodilla.ecommercee.exception.OrderNotFoundException;
+import com.kodilla.ecommercee.mapper.OrderMapper;
+import com.kodilla.ecommercee.service.OrderDbService;
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.util.Arrays;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/v1/orders")
+@Builder
+@RequiredArgsConstructor
 public class OrderController {
+    private final OrderMapper orderMapper;
+    private final OrderDbService orderDbService;
 
     @GetMapping
     public List<OrderDto> getOrders() {
-        return Arrays.asList(OrderDto.builder()
-                .orderId(1L)
-                .deliveryAddress("ul.Warszawska 21, 02-550 Warszawa")
-                .deliveryMethod("Courier GLS")
-                .orderData(LocalDateTime.now())
-                .value(new BigDecimal(200L))
-                .build(),
-
-        OrderDto.builder()
-                .orderId(2L)
-                .deliveryAddress("ul.Wrocławska 22, 03-220 Wrocław")
-                .deliveryMethod("Courier UPS")
-                .orderData(LocalDateTime.now())
-                .value(new BigDecimal(200L))
-                .build());
+        List<Order> orders = orderDbService.getALlOrders();
+        return orderMapper.mapToOrderDtoList(orders);
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public OrderDto createOrder(@RequestBody OrderDto orderDto) {
-        return OrderDto.builder()
-                .orderId(3L)
-                .deliveryAddress("ul.Poznańska 22, 03-220 Poznań")
-                .deliveryMethod("Courier UPS")
-                .orderData(LocalDateTime.now())
-                .value(new BigDecimal(200L))
-                .build();
+    public ResponseEntity<Void> createOrder(@RequestBody OrderDto orderDto) {
+        Order order = orderMapper.mapToOrder(orderDto);
+        orderDbService.saveOrder(order);
+        return ResponseEntity.ok().build();
     }
 
     @GetMapping(value = "/{orderId}")
-    public OrderDto getOrder(@PathVariable int orderId) throws OrderNotFoundException {
-        return OrderDto.builder()
-                .orderId(orderId)
-                .deliveryAddress("Warszawska 21, 02-550 Warszawa")
-                .deliveryMethod("Courier GLS")
-                .orderData(LocalDateTime.now())
-                .value(new BigDecimal(200L))
-                .build();
-
+    public ResponseEntity<OrderDto> getOrder(@PathVariable Long orderId) throws OrderNotFoundException {
+        return ResponseEntity.ok(orderMapper.mapToOrderDto(orderDbService.getOrder(orderId)));
     }
 
     @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public OrderDto updateOrder(@RequestBody OrderDto orderDto) throws OrderNotFoundException {
-        return orderDto;
+    public ResponseEntity<OrderDto> updateOrder(@RequestBody OrderDto orderDto) {
+        Order order = orderMapper.mapToOrder(orderDto);
+        Order saveOrder = orderDbService.saveOrder(order);
+        return ResponseEntity.ok(orderMapper.mapToOrderDto(saveOrder));
     }
 
     @DeleteMapping("/{orderId}")
-    public OrderDto deleteOrder(@PathVariable int taskId) {
-        return deleteOrder(taskId);
+    public ResponseEntity<Void> deleteOrder(@PathVariable Long orderId) throws OrderNotFoundException {
+        orderDbService.deleteOrder(orderId);
+        return ResponseEntity.ok().build();
     }
-
-
 }
