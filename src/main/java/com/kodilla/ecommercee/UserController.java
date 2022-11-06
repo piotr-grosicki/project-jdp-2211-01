2654574e544911ec690e21fffce4fb3533daaf5c
unknown <mateuszp.nowak@gmail.com>
2022-11-06 21:22:47 +0100

    User implementation methods transfer to userService

--- src/main/java/com/kodilla/ecommercee/UserController.java
+++ src/main/java/com/kodilla/ecommercee/UserController.java
@@ -3,22 +3,13 @@ package com.kodilla.ecommercee;
 
 import com.kodilla.ecommercee.domain.AuthDto;
 import com.kodilla.ecommercee.domain.UserDto;
-import com.kodilla.ecommercee.entity.User;
 import com.kodilla.ecommercee.exception.UserNotFoundException;
-import com.kodilla.ecommercee.mapper.UserMapper;
-import com.kodilla.ecommercee.repository.UserDao;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
+import com.kodilla.ecommercee.service.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.security.KeyPair;
-import java.time.Instant;
-import java.util.Date;
 
 
 @AllArgsConstructor
@@ -26,42 +17,26 @@ import java.util.Date;
 @RequestMapping("/v1/users")
 public class UserController {
 
-
-    private final UserMapper userMapper;
-
-    private final UserDao userDao;
+    private final UserService userService;
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> createUser(@RequestBody UserDto userDto) {
-        User user = userMapper.mapToUser(userDto);
-        userDao.save(user);
+        userService.createUser(userDto);
         return ResponseEntity.ok(null);
     }
 
 
     @PatchMapping(value = "/{userID}/blockUser")
     public ResponseEntity<Void> blockUser(@PathVariable long userID) throws UserNotFoundException {
-        User user = userDao.findById(userID).orElseThrow(UserNotFoundException::new);
-        user.setActive(false);
-        userDao.save(user);
+        userService.blockUser(userID);
         return ResponseEntity.ok(null);
     }
 
 
     @PostMapping(value = "/generateToken")
     public ResponseEntity<String> generateToken(@RequestBody AuthDto authDto) {
-        User user = userDao.findByLogin(authDto.getLogin()).orElse(null);
-        Date dateNow = Date.from(Instant.now());
-        if (user!=null && user.getPassword().equals(authDto.getPassword())) {
-            KeyPair keys = Keys.keyPairFor(SignatureAlgorithm.ES512);
-            return ResponseEntity.ok(Jwts.builder()
-                    .setSubject(user.getLogin())
-                    .setIssuedAt(dateNow)
-                    .setExpiration(Date.from(dateNow.toInstant().plusSeconds(3600)))
-                    .claim("roles", "user")
-                    .signWith(keys.getPrivate()).compact());
-        } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
+
+       return userService.generateToken(authDto);
+
     }
 }
