5d1c45e444db880146d87c2e19d54e02d7bbad73
Sebastian Boron <sebastian.boron8@gmail.com>
2022-11-09 12:44:40 +0100

    GlobalHttpErrorHandler fixed

--- src/main/java/com/kodilla/ecommercee/globalErrorHandler/GlobalHttpErrorHandler.java
+++ src/main/java/com/kodilla/ecommercee/globalErrorHandler/GlobalHttpErrorHandler.java
@@ -5,15 +5,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
 @ControllerAdvice
 public class GlobalHttpErrorHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ProductNotFoundException.class)
     public ResponseEntity<Object> handleProductNotFoundException(ProductNotFoundException e) {
-        return new ResponseEntity<>("Product not found", HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>("Product not found", HttpStatus.NOT_FOUND);
     }
 }
